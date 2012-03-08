c533687792b62abe2231ec6ea0af754291f7aa96
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2012-03-09 00:28:04 +0200

    precompile resource.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,8 @@ Appv3::Application.configure do
 
   # Generate digests for assets URLs
   config.assets.digest = true
+  
+  config.assets.precompile += %w( backend/resource.css )
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
