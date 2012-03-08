22ccb687670f81c25819b795ed29e15bf11420a6
Bahia ElSharkawy <bahia.sharkawy@gmail.com>
2012-03-08 21:03:09 +0200

    SiteURLChanged

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,4 @@
 APP_ID= '372795512732759' # please change!
 APP_SECRET= 'e1e9902c23b217bc04b04500fdbe94c2' # please change!
-SITE_URL = 'http://localhost:3000'
+#SITE_URL = 'http://localhost:3000'
+SITE_URL = 'http://adhocads.herokuapp.com/'
